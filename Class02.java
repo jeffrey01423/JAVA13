6c9c2b09cd6898b11593cd93da3447586ece217b
jeffrey01423 <89950800+jeffrey01423@users.noreply.github.com>
2022-01-09 21:14:52 +0800

    Update Class02.java

--- Class02.java
+++ Class02.java
@@ -2,17 +2,16 @@ public class JAVA1302 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
 		int num=12,den=0;
-		int ans=num/den;
-		System.out.println("ans="+ans);
-		}
-		catch(ArithmeticException e){
+		int ans;
+		if(den==0) {
 		System.out.println("除數為 0");
 		}
-		finally {
+		else {
+		ans=num/den;
+		System.out.println("ans="+ans);
+		}
 		System.out.println("end of main() method!!");
-		} 
 	}
 
 }
