265f3d8a8e7abf4a7c0411e7f0fd2e61bb71153d
jeffrey01423 <89950800+jeffrey01423@users.noreply.github.com>
2022-01-09 21:28:59 +0800

    Create Class06.java

---
+++ Class06.java
@@ -0,0 +1,16 @@
+public class JAVA1306 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			int num=12;
+			int den[]={12,0,3,0,0,4};
+			for(int i=0;i<10;i++) {
+				System.out.println("ans="+num/den[i]);
+			}
+		}
+		catch(Exception e){
+			System.out.println("捕捉到例外了");
+			}
+	}
+}
